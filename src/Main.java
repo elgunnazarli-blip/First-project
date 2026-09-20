ea8b184ad88b600ec733c93d7d3f807cd529cbbb
Elgun Nazarli <elgunnazarli@gmail.com>
2026-09-20 14:04:36 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Adınızı daxil edin? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Salam, " + name + "!");
+        scanner.close();
+
+        byte a = 6;
+        short b = 2;
+        int c = 12;
+        long d = 24;
+        float e = 12.3f;
+        double f = 20.2;
+
+        c = 30;
+        long g = (long) c;
+        System.out.println(g);
+        a = 45
+        short l = (short)
+    }
+}
