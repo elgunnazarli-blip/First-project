715d2a6ea85a315c4fabeac1b3c290b26022cda6
Elgun Nazarli <elgunnazarli@gmail.com>
2026-09-20 18:26:36 +0400

    Update Main.java

---
+++ src/insert.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class insert {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Give your name please:");
+        String name = n.nextLine();
+        System.out.println("Hello " + name);
+        Scanner a = new Scanner(System.in);
+        System.out.println("How old are you:");
+        int age = a.nextInt();
+        System.out.println(age);
+        if(age > 18){
+            System.out.println(name + " you can access to our website");
+        }
+        else{
+            System.out.println("Try again plaese");
+        }
+
+    }
+}
+
+
